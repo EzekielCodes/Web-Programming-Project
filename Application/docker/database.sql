dbb0ae3bc00563e30893b1bd24ee00924f30a21d
Toluwani Ezekiel Akindele <toluwani.akindele@student.odisee.be>
2022-11-29 16:07:12 +0100

    email added to register

--- Application/docker/database.sql
+++ Application/docker/database.sql
@@ -6,10 +6,11 @@ CREATE TABLE `users` (
     `id` INT NOT NULL  AUTO_INCREMENT,
     `username` VARCHAR(255) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
     `added_on` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci AUTO_INCREMENT = 4;
 
-INSERT INTO `users` VALUES (1,'Ezekiel', '$2y$10$YrY3/m/ry/hGAhVEcfWPrOgQgv.nO/nxSXJLP4vQP..stPK/2uSzC', now());
-INSERT INTO `users` VALUES (2,'Akindele', '$2y$10$YrY3/m/ry/hGAhVEcfWPrOgQgv.nO/nxSXJLP4vQP..stPK/2uSzC', now());
+INSERT INTO `users` VALUES (1,'Ezekiel', '$2y$10$YrY3/m/ry/hGAhVEcfWPrOgQgv.nO/nxSXJLP4vQP..stPK/2uSzC','test@gmail.com', now());
+INSERT INTO `users` VALUES (2,'Akindele', '$2y$10$YrY3/m/ry/hGAhVEcfWPrOgQgv.nO/nxSXJLP4vQP..stPK/2uSzC','testOne@gmail.com', now());
 
--- Application/docker/webcontent/register.php
+++ Application/docker/webcontent/register.php
@@ -22,8 +22,8 @@ require_once 'includes/functions.php';
     
  
 // Define variables and initialize with empty values
-$username = $password = $confirm_password = "";
-$username_err = $password_err = $confirm_password_err = "";
+$username = $password = $email = $confirm_password = "";
+$username_err = $password_err = $email_err =  $confirm_password_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
@@ -59,6 +59,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             unset($stmt);
         }
     }
+
+
+
+     // Validate email
+     if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter a email.";
+        } elseif(!filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)){
+            $email_err = "email can only contain letters, numbers, and underscores.";
+        } else{
+            // Prepare a select statement
+            $sql = "SELECT id FROM users WHERE email = :email";
+            
+            if($stmt = $pdo->prepare($sql)){
+                // Bind variables to the prepared statement as parameters
+                $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
+                
+                // Set parameters
+                $param_email = trim($_POST["email"]);
+                
+                // Attempt to execute the prepared statement
+                if($stmt->execute()){
+                    if($stmt->rowCount() == 1){
+                        $email_err = "This email is already taken.";
+                    } else{
+                        echo 'happe';
+                        $email = trim($_POST["email"]);
+                    }
+                } else{
+                    echo "Oops! Something went wrong. Please try again later.";
+                }
+
+                // Close statement
+                unset($stmt);
+            }
+    }
     
     // Validate password
     if(empty(trim($_POST["password"]))){
@@ -80,19 +115,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     }
     
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $sql = "INSERT INTO users (username, password,email) VALUES (:username, :password,:email)";
          
         if($stmt = $pdo->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
             $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+            $stmt->bindParam(":email", $param_email, PDO::PARAM_STR);
             
+            echo $email;
             // Set parameters
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_email = $email;
             
             // Attempt to execute the prepared statement
             if($stmt->execute()){
@@ -137,6 +175,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <label>Password</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
                 <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
             </div>
             <div class="form-group">
                 <label>Confirm Password</label>
